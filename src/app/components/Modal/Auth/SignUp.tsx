a1febfce115afbff420b3e0c7d8792d25ad2e5ca
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-03-24 17:26:49 +0100

    add createUser firebase hook for a sign-up with email and password

--- src/app/components/Modal/Auth/SignUp.tsx
+++ src/app/components/Modal/Auth/SignUp.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Button, Flex, Input, Text } from '@chakra-ui/react';
 import { useSetRecoilState } from 'recoil';
 import { authModalState } from '@/atoms/authModalAtom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 const SignUp:React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -11,7 +13,22 @@ const SignUp:React.FC = () => {
     confirmPassword: "",
   });
 
-  const onSubmit = () => {}; // TODO: add firebase logic
+  const [ error, setError ] = useState("");
+  const [ createUserWithEmailAndPassword, user, loading, userError ] = useCreateUserWithEmailAndPassword(auth);
+
+  // Firebase logic
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    if (error) setError("");
+    if (signUpForm.password !== signUpForm.confirmPassword) {
+      setError("Passwords do not match");
+    };
+    /* TODO: add more validation later (min password length, special characters, email domain, outline incorrect field, disable submit button, etc.) */
+
+    createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
+  };
+
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
     
     setSignUpForm(prev => ({
@@ -88,6 +105,12 @@ const SignUp:React.FC = () => {
         }}
         bg="gray.50"
       />
+      {error || 
+        (userError && (
+          <Text color="red" fontSize="10pt" mb={2} ml={4}>
+            {error || userError.message}
+          </Text>
+        ))}
       <Button 
         type="submit"
         width="100%"
