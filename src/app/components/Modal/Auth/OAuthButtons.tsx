fbd55d6d7b6dd53e96bc321321c3726d0e8baf04
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-03-24 19:51:39 +0100

    add firebase hook for a sign-in with google

--- src/app/components/Modal/Auth/OAuthButtons.tsx
+++ src/app/components/Modal/Auth/OAuthButtons.tsx
@@ -1,11 +1,20 @@
-import { Button, Flex, Image, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Button, Flex, Image, Text } from '@chakra-ui/react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
+// both for login and signup modals
 const OAuthButtons:React.FC = () => {
-  
+  const [ signInWithGoogle, user, loading, error ] = useSignInWithGoogle(auth);
+
   return (
   <Flex direction="column" width="100%" mb={4}>
-    <Button variant="oauth" mb={2}>
+    <Button 
+      variant="oauth" 
+      mb={2} 
+      isLoading={loading} 
+      onClick={() => signInWithGoogle()}
+    >
       <Image 
         src="/images/googlelogo.png" 
         alt="Google logo" 
@@ -23,6 +32,7 @@ const OAuthButtons:React.FC = () => {
         />
       <Text flexGrow={1}>Continue with Other</Text>
     </Button>
+    {error && <Text color="red" fontSize="10pt" mt={2} ml={5}>{error.message}</Text>}
   </Flex>
   );
 }
