881b166869894f529bb93c11879bd4857cfa9ea3
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-03-31 18:03:25 +0200

    add log out logic

--- src/app/components/Navbar/RightContent/index.tsx
+++ src/app/components/Navbar/RightContent/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import AuthButtons from './AuthButtons';
 import AuthModal from '../../Modal/Auth';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/firebase/clientApp';
 
 type RightContentProps = {
-  // user: any;
+  user: any;
 };
 
-const RightContent:React.FC<RightContentProps> = () => {
-  
+// `{ user }` is the same as `props.user`
+const RightContent:React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
     <AuthModal />
     <Flex justify="center" align="center">
-      <AuthButtons />
+      {user ? <Button onClick={() => signOut(auth)}>Log Out</Button> : <AuthButtons />}
     </Flex>
     </>
   );
--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -4,8 +4,12 @@ import React from 'react';
 import { Flex, Image } from '@chakra-ui/react';
 import SearchInput from './SearchInput';
 import RightContent from './RightContent';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 const Navbar:React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
       <Flex align="center">
@@ -22,7 +26,7 @@ const Navbar:React.FC = () => {
         />
       </Flex>
       <SearchInput />
-      <RightContent />
+      <RightContent user={user} />
       
       {/* 
         <Directory /> –– dropdown menu
