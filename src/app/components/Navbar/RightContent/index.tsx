4d09d8504f4ba33dd84026a6a6ec2a18a15a9f10
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-04-04 16:44:33 +0200

    minor code refactoring

--- src/app/components/Navbar/RightContent/index.tsx
+++ src/app/components/Navbar/RightContent/index.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Flex } from '@chakra-ui/react';
-import AuthButtons from './AuthButtons';
-import AuthModal from '../../Modal/Auth';
-import { User } from 'firebase/auth';
-import Icons from './Icons';
-import UserMenu from './UserMenu';
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import AuthModal from "../../Modal/Auth";
+import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
+import UserMenu from "./UserMenu";
 
 type RightContentProps = {
   user?: User | null;
 };
 
-const RightContent:React.FC<RightContentProps> = ({ user }) => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
-    <AuthModal />
-    <Flex justify="center" align="center">
-      {user ? (<Icons />) : (<AuthButtons />)}
-      <UserMenu user={user} />
-    </Flex>
+      <AuthModal />
+      <Flex justify="center" align="center">
+        {user ? <Icons /> : <AuthButtons />}
+        <UserMenu user={user} />
+      </Flex>
     </>
   );
-}
+};
+
 export default RightContent;
