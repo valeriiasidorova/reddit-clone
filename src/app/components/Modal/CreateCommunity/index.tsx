39bd38348090c448e01745e56fa7a1e611ad1a21
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-04-05 17:25:46 +0200

    add remaining characters tracker for a community name input

--- src/app/components/Modal/CreateCommunity/index.tsx
+++ src/app/components/Modal/CreateCommunity/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -7,7 +7,11 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Text,
   Button,
+  Box,
+  Divider,
+  Input,
 } from "@chakra-ui/react";
 
 type CreateCommunityModalProps = {
@@ -19,15 +23,64 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const [communityName, setCommunityName] = useState("");
+  const [charsRemaining, setCharsRemaining] = useState(21);
+
+  // recalculate how many characters left
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 21) return;
+
+    setCommunityName(event.target.value);
+    setCharsRemaining(21 - event.target.value.length);
+  };
+
   return (
     <>
       <Modal isOpen={open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create a community</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>modal body</ModalBody>
-
+          <ModalHeader
+            display="flex"
+            flexDirection="column"
+            fontSize={15}
+            padding={3}
+          >
+            Create a community
+          </ModalHeader>
+          <Box pl={3} pr={3}>
+            <Divider />
+            <ModalCloseButton />
+            <ModalBody display="flex" flexDirection="column" padding="10px 0">
+              <Text fontWeight={600} fontSize={15}>
+                Name
+              </Text>
+              <Text fontSize={11} color="gray.500">
+                Community names including capitalization cannot be changed
+              </Text>
+              <Text
+                position="relative"
+                top="28px"
+                left="10px"
+                width="20px"
+                color="gray.400"
+              >
+                r/
+              </Text>
+              <Input
+                value={communityName}
+                size="sm"
+                pl="22px"
+                onChange={handleChange}
+              />
+              <Text
+                fontSize="9pt"
+                mt={1}
+                color={charsRemaining === 0 ? "red" : "gray.500"}
+              >
+                {charsRemaining} characters remaining
+              </Text>
+            </ModalBody>
+          </Box>
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={handleClose}>
               Close
