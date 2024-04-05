d2f672067302b4b5976203e84724f8290b12a761
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-04-05 16:09:17 +0200

    fix navbar styles

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,36 +1,46 @@
-'use client'
+"use client";
 
-import React from 'react';
-import { Flex, Image } from '@chakra-ui/react';
-import SearchInput from './SearchInput';
-import RightContent from './RightContent';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/clientApp';
-import Directory from './Directory';
+import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
+import Directory from "./Directory";
 
-const Navbar:React.FC = () => {
+const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
 
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
-        <Image 
-          src="/images/redditHead.svg" 
-          alt="Logo: Reddit mascot in an orange-red bubble" 
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+      >
+        <Image
+          src="/images/redditHead.svg"
+          alt="Logo: Reddit mascot in an orange-red bubble"
+          // width={{ base: "25px", sm:"30px", md: "auto" }}
           height="30px"
         />
-        <Image 
-          src="/images/redditText.svg" 
-          alt="Reddit text logo" 
+        <Image
+          src="/images/redditText.svg"
+          alt="Reddit text logo"
           height="46px"
-          display={{ base: "none", md: "unset" }} 
+          display={{ base: "none", md: "unset" }}
         />
       </Flex>
       {user && <Directory />}
-      <SearchInput />
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
-  )
-}
+  );
+};
 
 export default Navbar;
